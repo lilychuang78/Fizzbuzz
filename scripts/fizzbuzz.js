3de06e33a669da4bb6de4e3bbfee489a1129547f
lilychuang78 <lilychuang78@gmail.com>
2020-09-03 10:21:13 +0000

    modifications

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,11 +1,11 @@
-whatCanIdrink = function(number) {
+FizzBuzz = function(number) {
     if(number%3 == 0 && number%5==0){
         return "FizzBuzz";
     }
-    if(number%3 == 0){
+    else if(number%3 == 0){
         return "Fizz";
     }
-    if(number%5 == 0){
+    else if(number%5 == 0){
         return "Buzz";
     }
 };
--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,24 +1,24 @@
 describe("number devided by 3 or 5", function() {
     beforeEach(function(){
-        fizzbuzz = new fizzbuzz;
+        fizzbuzz = new FizzBuzz;
     });
 
 
     describe("enter number", function() {
         it("should exist", function() {
-            expect(fizzbuzz).toBeDefined();
+            expect(FizzBuzz).toBeDefined();
         });
 
         it("should return Fizzbuzz when fizzbuzz(15)", function() {
-           var number = fizzbuzz(15)
-            expect(number).toBe("Fizzbuzz");
+           var number = FizzBuzz(15)
+            expect(number).toBe("FizzBuzz");
         });
-        it("should return Fizz when fizzbuzz(3)", function() {
-           var number = fizzbuzz(3)
+        it("should return Fizz when FizzBuzz(3)", function() {
+           var number = FizzBuzz(3)
             expect(number).toBe("Fizz");
         });
-        it("should return Buzz when fizzbuzz(10)", function() {
-           var number = fizzbuzz(10)
+        it("should return Buzz when FizzBuzz(10)", function() {
+           var number = FizzBuzz(10)
             expect(number).toBe("Buzz");
         });
     });
